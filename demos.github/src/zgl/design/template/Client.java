4238e88f7583ad0d8695294764df0ed376899ca2
thithinker <thithinker@gmail.com>
2013-12-07 19:54:21 +0800

    2013.12.07

--- demos.github/src/zgl/design/template/Client.java
+++ demos.github/src/zgl/design/template/Client.java
@@ -4,5 +4,9 @@ public class Client {
 	public static void main(String[] args){
 		HummerModel h = new HummerH1Model();
 		h.run();
+		
+		HummerModel h2 = new HummerH2Model();
+		h2.run();
+		
 	}
 }
